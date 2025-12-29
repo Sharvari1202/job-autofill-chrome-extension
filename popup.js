b6b112bca2b6f87f5597f7e93331e4ac104d85d7
Sharvari Jadhav <your-github-jadhavsharvari588@gmail.com>
2025-12-29 19:21:49 +0530

    Initial commit - Job Autofill Chrome Extension

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.getElementById("autofill").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "AUTOFILL_FORM" });
+  });
+});
